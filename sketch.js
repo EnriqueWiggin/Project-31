19cbabb76af4f15071222bdd6b464661612184eb
EnriqueWiggin <70081772+EnriqueWiggin@users.noreply.github.com>
2020-10-29 21:06:54 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -15,24 +15,37 @@ function preload()
 }
 
 function setup() {
-	createCanvas(480, 800);
+	createCanvas(800, 800);
 
 
 	engine = Engine.create();
 	world = engine.world;
 
-	ground = new Ground(450,390,900,20);
+	ground = new Ground(width/2,height,width,20);
+	
 	for(var k = 0; k <=width; k = k + 80) {
 		divisions.push(new Division(k, height - divisionHeight/2, 10, divisionHeight));
 	}
-	for(var j=40; j<=width; j=j+50)
+	for(var j=75; j<=width; j=j+50)
 	{
 		plinkos.push(new Plinko(j,75));
 	}
-	for(var j = 15; j<=width-10; j=j+50)
+	for(var j = 50; j<=width-10; j=j+50)
 	{
 		plinkos.push(new Plinko(j,175));
 	}
+	for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,275));
+	}
+
+	if(frameCount%60===0) {
+		particles.push (new Particle (random(width/2-10,width/2+10),10,10));
+	}
+	
+
+	
 
 
 	Engine.run(engine);
@@ -45,6 +58,21 @@ function draw() {
   background(0);
 
   ground.display();
+
+  for (var i = 0; i < plinkos.length; i++) {
+     
+	plinkos[i].display();
+	
+  }
+  
+  for (var j = 0; j < particles.length; j++) {
+		
+	particles[j].display();
+}
+for (var k = 0; k < divisions.length; k++) {
+
+	divisions[k].display();
+}
   
   drawSprites();
   
